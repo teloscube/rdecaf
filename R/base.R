e6b888e3c27b1f259cf5bf74d4f3338cbae8f3fb
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2019-05-08 18:18:33 +0800

    [improve] Add multipart form (for file uploads)

--- R/base.R
+++ R/base.R
@@ -241,3 +241,51 @@ deleteResource <- function (..., params=list(), session=NULL) {
     ## Return:
     httr::content(response)
 }
+
+
+##' Uploads a resource to DECAF API.
+##'
+##' This is a variant of postResource method.
+##'
+##' @param ... URL path segments.
+##' @param params Query parameters.
+##' @param payload The payload to be send.
+##' @param session Session information.
+##' @return Parsed R object for the response.
+##'
+##' @import httr
+##' @import readr
+##' @export
+postMultipart <- function (..., params=list(), payload=NULL, session=NULL) {
+    ## Get or create a session:
+    if (is.null(session)) {
+        session <- readSession()
+    }
+
+    ## Get the base url to start to build the endpoint URL:
+    url <- httr::parse_url(session$location)
+
+    ## Add paths ensuring that path seperator is not duplicated and a
+    ## trailing path seperator is added:
+    url$path <- c(sub("/$", "", gsub("//", "/", c(url$path, ...))), "/")
+
+    ## Add params:
+    url$query <- params
+
+    ## Construct the endpoint URL:
+    url <- httr::build_url(url)
+
+    ## Post the resource:
+    response <- httr::POST(url, httr::add_headers(Authorization=.authorizationHeader(session)), body=payload, encode="multipart")
+
+    ## Get the status:
+    status <- response$status_code
+
+    ## If the status code is not 200, raise an error:
+    if (status >= 300) {
+        stop(sprintf("%s returned a status code of '%d'.\n\n  Details provided by the API are:\n\n%s", url, status, httr::content(response, as="text")))
+    }
+
+    ## Return:
+    httr::content(response)
+}
